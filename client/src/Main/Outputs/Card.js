380975b0f91bb909a613e5ac518aa539427d62af
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2022-06-25 20:24:45 +0800

    card elements made

--- client/src/Main/Outputs/Card.js
+++ client/src/Main/Outputs/Card.js
@@ -1,7 +1,16 @@
-import { CardContainer } from "./styles"
+import { CardContainer, Establishment, BlurbContainer, Address, CodeContainer, Code } from "./styles"
 
 const Card = () => {
     return <CardContainer>
+        <BlurbContainer>
+
+        <Establishment>Starbucks</Establishment>
+        <Address>2931 W. Hemlock St. Apt C, Oxnard, CA</Address>
+        </BlurbContainer>
+
+        <CodeContainer>
+            <Code>4351234</Code>
+        </CodeContainer>
     </CardContainer>
 }
 
--- client/src/Main/Outputs/styles.js
+++ client/src/Main/Outputs/styles.js
@@ -5,13 +5,46 @@ export const MainContainer = styled.div`
   padding: 20px;
   display: grid;
   grid-row-gap: 20px;
-  grid-auto-rows: 150px;
-  
+  grid-auto-rows: auto;
 `;
+
+// NOTE ---------------------------------------------------------CARD STYLING
 
 export const CardContainer = styled.div`
   border-radius: 20px;
   background-size: 30%;
   background: #ffffff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px 20px;
 `;
 
+export const BlurbContainer = styled.div`
+
+
+`;
+
+export const CodeContainer = styled.div`
+  
+`;
+
+export const Establishment = styled.h2`
+  font-size: 32px;
+  line-height: 32px;
+  margin-bottom: 8px;
+  font-weight: 800;
+`;
+
+export const Address = styled.h3`
+  font-size: 18px;
+  line-height: 18px;
+  font-weight: 300;
+  opacity: 0.4;
+`;
+
+export const Code = styled.h2`
+  font-size: 24px;
+  line-height: 24px;
+  font-weight: 500;
+`;
